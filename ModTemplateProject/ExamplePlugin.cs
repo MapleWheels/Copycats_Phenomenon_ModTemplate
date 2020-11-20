ab11486317afbff55d5b0511eb97a84cf5fa72c3
Narayan Jainanan <njainanan@hotmail.com>
2020-11-20 16:39:26 -0500

    Initial Commit.

---
+++ ModTemplateProject/ExamplePlugin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BepInEx;
+
+namespace ModTemplateProject
+{
+    [BepInPlugin("com.test.exampleplugin", "Example Plugin", "0.0.0")]
+    public class ExamplePlugin : BaseUnityPlugin
+    {
+        void Awake()
+        {
+            Logger.LogWarning("Plugin loaded.");
+            Logger.LogError("Plugin loaded.");
+            Logger.LogInfo("Plugin loaded.");
+            Logger.LogDebug("Plugin loaded.");
+
+            UnityEngine.Debug.LogWarning("Example Plugin loaded.");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+This is a template project setup with the references/dependencies for modding the game [Copycat's Phenomenon](https://gamejolt.com/games/phenomenon/364677). 
+
+Includes:
+ - Stubbed and fully publicized assemblies for UnityEngine, Mirror API and the game's Assembly.
+ - Configured .gitignore.
+ - [BepInEx.Extensions](https://github.com/MapleWheels/BepInEx_Extensions) configuration.
+ - Setup for use with [BepInEx 5.4.x](https://github.com/BepInEx/BepInEx).
+ 
+All you need to do is rename the solution and the project and follow standard BepInEx plugin installation instructions.
